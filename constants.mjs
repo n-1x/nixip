ba7f4f6cd56da1056c98353a0ed4ef036e1dc7e8
n-1x <0@00.lu>
2022-12-31 01:37:50 +0000

    Switched to ZIP64 format and added compression support. Use file descriptors.

--- constants.mjs
+++ constants.mjs
@@ -1,9 +1,18 @@
 export const Signatures = {
     centralDirectoryHeader: 0x02014b50,
     localFileHeader: 0x04034b50,
-    endOfCentralDirectory: 0x06054b50
+    endOfCentralDirectory: 0x06054b50,
+    zip64EndOfCentralDirectory: 0x06064b50,
+    zip64EndOfCentralDirectoryLocator: 0x07064b50,
+    dataDescriptor: 0x08074b50
 };
 
 export const CompressionMethods = {
-    store: 0 // no compression
+    store: 0, // no compression
+    deflate: 8
+};
+
+export const Flags = {
+    Descriptor: 1 << 3,
+    UTF8: 1 << 11
 };
